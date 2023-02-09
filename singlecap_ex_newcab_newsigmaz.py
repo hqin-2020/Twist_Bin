8517c07f8ab9dd245f0c67320b59846792d8dcc9
hqin-2020 <uctphqi@ucl.ac.uk>
2023-02-09 14:07:04 -0600

    new commit

--- singlecap_ex_newcab_newsigmaz.py
+++ singlecap_ex_newcab_newsigmaz.py
@@ -37,17 +37,14 @@ args = parser.parse_args()
 alpha_z_hat = 0.0
 kappa_hat = 0.014
 
-alpha_k_hat = -0.88       # -1.279
-alpha_c_hat = 0.484      
+alpha_k_hat = -0.0088       # -1.279  
 
 beta_hat = 1.0
-sigma_c = np.array([0.477, 0.0 ])   # consumption exposure (= exposure of single capital)
+sigma_c = 0.01*np.array([0.477, 0.0 ])   # consumption exposure (= exposure of single capital)
 
-sigma_z = np.array([0.011, 0.025])
+sigma_z = 0.01*np.array([0.011, 0.025])
 rho = args.rho
 
-# delta = 0.002
-# A_cap = 0.05
 
 # delta = 0.002 # 0.0025
 delta = 0.0025 # 0.0025
@@ -141,8 +138,6 @@ FC_Err = 1
 epoch = 0
 max_iter = args.maxiter
 tol = 1e-6
-# fraction = 0.1
-# epsilon = 0.01
 fraction = args.fraction
 epsilon = args.epsilon
 
@@ -193,7 +188,7 @@ while FC_Err > tol and epoch < max_iter:
     C_3 = np.zeros(W1_mat.shape)
     temp = (1-rho)* ( np.log(A_cap - d)-V0 )
     D = delta/(1-rho) * ( np.exp(temp) - 1) 
-    D += theta1*np.log(1+theta2*d) + .01 * (alpha_k_hat + beta_hat *W1_mat - .01/2*(sigma_c[0]**2+sigma_c[1]**2) ) +.01 * (sigma_c[0]*h1 + sigma_c[1]*hz)
+    D += theta1*np.log(1+theta2*d) + (alpha_k_hat + beta_hat *W1_mat - 1/2*(sigma_c[0]**2+sigma_c[1]**2) ) + (sigma_c[0]*h1 + sigma_c[1]*hz)
     D += ell * ( h1**2+hz**2 )/2
     
     start_ksp = time.time()
